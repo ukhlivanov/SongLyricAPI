a82564783b1fbd7ebe5a368a2830f7ed31325cef
Sergey Ukhlivanov <ukhlivanov@gmail.com>
2018-07-04 12:58:29 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function getDataFromApi(artist, title, callback) {
+  //your code here
+}
+
+function displaySearchData(data) {
+  //your code here
+}
+
+function watchSubmit() {
+  //your code here
+}
+
+$(watchSubmit);
